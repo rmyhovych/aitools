8dd7062ecf20b0b6b4724fe43110c3082e0279e4
ross <rmyhovych@gmail.com>
2020-08-16 17:37:14 -0400

    use jit Script Module for LSTM

--- library/aitools/nn/rnn/lstm.py
+++ library/aitools/nn/rnn/lstm.py
@@ -1,78 +1,61 @@
 import torch
+from torch.nn import Linear as L
 
 
-class NetworkLSTM(torch.nn.Module):
-    def __init__(self, inSize, hiddenSize, outSize, outAct):
+class CellLSTM(torch.jit.ScriptModule):
+    def __init__(self, input_size, hidden_size):
+        super(CellLSTM, self).__init__()
+        self.hidden_size = hidden_size
+
+        interm_size = input_size + hidden_size
+        self.forget_layer = L(interm_size, hidden_size)
+
+        self.update_layer_force = L(interm_size, hidden_size)
+        self.update_layer_direction = L(interm_size, hidden_size)
+
+        self.output_layer = L(interm_size, hidden_size)
+
+    @torch.jit.script_method
+    def forward(self, x, h, state):
+        xh = torch.cat((x, h), dim=0)
+
+        new_state = torch.mul(self._forget(xh), state)
+        new_state = torch.add(self._update(xh), new_state)
+        new_h = torch.mul(self._output(xh), torch.tanh(new_state))
+
+        return new_h, new_state
+
+    def _forget(self, xh):
+        return torch.sigmoid(self.forget_layer(xh))
+
+    def _update(self, xh):
+        force = torch.sigmoid(self.update_layer_force(xh))
+        direction = torch.tanh(self.update_layer_direction(xh))
+        return torch.mul(force, direction)
+
+    def _output(self, xh):
+        return torch.sigmoid(self.output_layer(xh))
+
+
+class NetworkLSTM(torch.jit.ScriptModule):
+    def __init__(self, input_size, hidden_size, output_size, output_activation=None):
         super(NetworkLSTM, self).__init__()
-        self.parameterList = torch.nn.ModuleList()
 
-        catInSize = inSize + hiddenSize
-        self.forgetGate = self._createForgetGate(catInSize, hiddenSize)
-        self.updateGate = self._createUpdateGate(catInSize, hiddenSize)
-        self.outputGate = self._createOutputGate(catInSize, hiddenSize)
-        self.outputLayer = self._createOutputLayer(hiddenSize, outSize, outAct)
+        self.cell = CellLSTM(input_size, hidden_size)
+        self.output_layer = L(hidden_size, output_size)
 
-        self.hidden = torch.zeros(size=(hiddenSize,))
-        self.cellState = torch.zeros(size=(hiddenSize,))
+        self.output_activation = (
+            torch.tensor if output_activation is None else output_activation
+        )
 
-    def reset(self):
-        self.hidden = torch.zeros(size=self.hidden.size())
-        self.cellState = torch.zeros(size=self.cellState.size())
+    @torch.jit.script_method
+    def forward(self, xs):
+        h, state = (
+            torch.zeros((self.cell.hidden_size,)),
+            torch.zeros((self.cell.hidden_size,)),
+        )
 
-    def forward(self, x):
-        xh = torch.cat((x, self.hidden), dim=0)
+        for x in xs:
+            h, state = self.cell(x, h, state)
 
-        forget = self.forgetGate(xh)
-        self.cellState = torch.mul(forget, self.cellState)
-
-        update = self.updateGate(xh)
-        self.cellState = torch.add(update, self.cellState)
-
-        self.hidden = self.outputGate(xh, self.cellState)
-
-        return self.outputLayer(self.hidden)
-
-    def _createForgetGate(self, inSize, outSize):
-        layer = torch.nn.Linear(inSize, outSize, bias=True)
-        self.parameterList.append(layer)
-
-        def forgetGate(xh):
-            return torch.sigmoid(layer(xh))
-
-        return forgetGate
-
-    def _createUpdateGate(self, inSize, outSize):
-        layerInput = torch.nn.Linear(inSize, outSize, bias=True)
-        self.parameterList.append(layerInput)
-
-        layerAdd = torch.nn.Linear(inSize, outSize, bias=True)
-        self.parameterList.append(layerAdd)
-
-        def updateGate(xh):
-            outInput = torch.sigmoid(layerInput(xh))
-            outAdd = torch.tanh(layerAdd(xh))
-
-            return torch.mul(outInput, outAdd)
-
-        return updateGate
-
-    def _createOutputGate(self, inSize, outSize):
-        layerOutput = torch.nn.Linear(inSize, outSize, bias=True)
-        self.parameterList.append(layerOutput)
-
-        def outputGate(xh, cellState):
-            out = torch.sigmoid(layerOutput(xh))
-            cellStateTanh = torch.tanh(cellState)
-
-            return torch.mul(cellStateTanh, out)
-
-        return outputGate
-
-    def _createOutputLayer(self, inSize, outSize, outAct):
-        layer = torch.nn.Linear(inSize, outSize, bias=True)
-        self.parameterList.append(layer)
-
-        if outAct is None:
-            return lambda hidden: layer(hidden)
-        else:
-            return lambda hidden: outAct(layer(hidden))
+        return self.output_activation(self.output_layer(h))
