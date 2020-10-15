271c73301a729119310ef7d7b241d5c639e7190d
rmyhovych <rmyhovych@gmail.com>
2020-10-15 16:42:21 -0400

    add hidden structure to lstm modules

--- library/aitools/nn/rnn/lstm.py
+++ library/aitools/nn/rnn/lstm.py
@@ -1,22 +1,27 @@
 import torch
-from torch.nn import Linear
 from typing import List, Tuple, Callable
 
 from ..network_ff import NetworkFF
 
 
 class CellLSTM(torch.nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(
+        self,
+        input_size: int,
+        memory_size: int,
+        hidden_sizes: List[Tuple[int, Callable[[torch.Tensor], torch.Tensor]]],
+    ):
         super(CellLSTM, self).__init__()
-        self.hidden_size = hidden_size
+        self.memory_size = memory_size
 
-        sub_input_size = input_size + hidden_size
-        self.forget_layer = Linear(sub_input_size, hidden_size)
+        sub_input_size = input_size + memory_size
 
-        self.update_layer_force = Linear(sub_input_size, hidden_size)
-        self.update_layer_direction = Linear(sub_input_size, hidden_size)
+        self.forget_module = self._build_module(sub_input_size, hidden_sizes + [memory_size, torch.sigmoid])
 
-        self.output_layer = Linear(sub_input_size, hidden_size)
+        self.update_module_force = self._build_module(sub_input_size, hidden_sizes + [memory_size, torch.sigmoid])
+        self.update_module_direction = self._build_module(sub_input_size, hidden_sizes + [memory_size, torch.tanh])
+
+        self.output_module = self._build_module(sub_input_size, hidden_sizes + [memory_size, torch.sigmoid])
 
     def forward(self, x, h, state):
         xh = torch.cat((x, h))
@@ -28,35 +33,42 @@ class CellLSTM(torch.nn.Module):
         return new_h, new_state
 
     def _forget(self, xh):
-        return torch.sigmoid(self.forget_layer(xh))
+        return self.forget_module(xh)
 
     def _update(self, xh):
-        force = torch.sigmoid(self.update_layer_force(xh))
-        direction = torch.tanh(self.update_layer_direction(xh))
+        force = self.update_module_force(xh)
+        direction = self.update_module_direction(xh)
         return torch.mul(force, direction)
 
     def _output(self, xh):
-        return torch.sigmoid(self.output_layer(xh))
+        return self.output_module(xh)
+
+    def _build_module(self, in_size: int, layers: List[Tuple[int, Callable[[torch.Tensor], torch.Tensor]]]) -> NetworkFF:
+        builder = NetworkFF.Builder(in_size)
+        for layer in layers:
+            builder.add_layer(layer[0], layer[1])
+        return builder.build()
 
 
 class NetworkLSTM(torch.nn.Module):
     def __init__(
         self,
         input_size: int,
-        hidden_size: int,
-        output_layers: List[Tuple[int, Callable[[torch.Tensor], torch.Tensor]]],
+        memory_size: int,
+        output_layer: Tuple[int, Callable[torch.Tensor], torch.Tensor],
         *,
+        hidden_sizes=[],
         device=None
     ):
         super(NetworkLSTM, self).__init__()
         self.device = device
 
-        self.cell = CellLSTM(input_size, hidden_size)
-
-        output_net_builder = NetworkFF.Builder(hidden_size)
-        for layer in output_layers:
-            output_net_builder.add_layer(layer[0], layer[1])
-        self.output_net = output_net_builder.build()
+        self.cell = CellLSTM(input_size, memory_size, hidden_sizes)
+        self.output_net = (
+            NetworkFF.Builder(memory_size)
+            .add_layer(output_layer[0], output_layer[1])
+            .build()
+        )
 
         self.to(self.device)
 
@@ -70,6 +82,6 @@ class NetworkLSTM(torch.nn.Module):
 
     def _create_new_states(self):
         return (
-            torch.zeros((self.cell.hidden_size,), device=self.device),
-            torch.zeros((self.cell.hidden_size,), device=self.device),
+            torch.zeros((self.cell.memory_size,), device=self.device),
+            torch.zeros((self.cell.memory_size,), device=self.device),
         )
